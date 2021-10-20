4bf4da244fce8ca5fa37b3c22084ea23a976e7d3
PAPADIMITRAKOPOULOS Vasilis <vasilis.papadimitrakopoulos@sword-group.com>
2021-10-20 18:36:53 +0300

    set up project

---
+++ src/main/java/com/udemy/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.udemy.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWord(){
+        return "Hello World";
+    }
+}
