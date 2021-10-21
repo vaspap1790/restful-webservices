f3a1787d4e97ab8f4ad8477a7c2ac2a0b771cff8
PAPADIMITRAKOPOULOS Vasilis <vasilis.papadimitrakopoulos@sword-group.com>
2021-10-21 14:16:08 +0300

    User bean, service and controller

---
+++ src/main/java/com/udemy/rest/webservices/restfulwebservices/user/UserController.java
@@ -0,0 +1,39 @@
+package com.udemy.rest.webservices.restfulwebservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserDaoService userDaoService;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return userDaoService.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user){
+
+        User savedUser = userDaoService.saveUser(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+}
