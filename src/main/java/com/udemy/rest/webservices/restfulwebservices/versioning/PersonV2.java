1020418df33c958fc3cd3e5c81a478c15575607d
PAPADIMITRAKOPOULOS Vasilis <vasilis.papadimitrakopoulos@sword-group.com>
2021-10-25 16:57:31 +0300

    api basic versioning

---
+++ src/main/java/com/udemy/rest/webservices/restfulwebservices/versioning/PersonV2.java
@@ -0,0 +1,18 @@
+package com.udemy.rest.webservices.restfulwebservices.versioning;
+
+public class PersonV2 {
+
+    private Name name;
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+}
