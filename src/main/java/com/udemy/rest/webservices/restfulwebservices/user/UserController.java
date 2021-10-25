bbf1b56f274303dc045561e2e95e9f1e3e7b2048
PAPADIMITRAKOPOULOS Vasilis <vasilis.papadimitrakopoulos@sword-group.com>
2021-10-25 11:25:32 +0300

    hateoas

--- src/main/java/com/udemy/rest/webservices/restfulwebservices/user/UserController.java
+++ src/main/java/com/udemy/rest/webservices/restfulwebservices/user/UserController.java
@@ -1,6 +1,9 @@
 package com.udemy.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -33,14 +36,18 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id){
 
         User user = userDaoService.findOne(id);
         if(user == null){
             throw new UserNotFoundException("User with id " + id + " does not exist!");
         }
 
-        return user;
+        EntityModel<User> model = EntityModel.of(user);
+        WebMvcLinkBuilder linkToUsers = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkToUsers.withRel("all-users"));
+
+        return model;
     }
 
     @DeleteMapping("/users/{id}")
