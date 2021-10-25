6a533bab5d2e388b667c237e2762db52f038aff3
PAPADIMITRAKOPOULOS Vasilis <vasilis.papadimitrakopoulos@sword-group.com>
2021-10-25 12:04:08 +0300

    internationalization

--- src/main/java/com/udemy/rest/webservices/restfulwebservices/helloWorld/HelloWorldController.java
+++ src/main/java/com/udemy/rest/webservices/restfulwebservices/helloWorld/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.udemy.rest.webservices.restfulwebservices.helloWorld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping("/hello-world/{name}")
     public String helloWord(@PathVariable String name){
         return "Hello World " + name;
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWordInternationalized(){
+        return messageSource.getMessage("good.morning.message", null, "Default Message", LocaleContextHolder.getLocale());
     }
 }
